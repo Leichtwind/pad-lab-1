d342db880cac90e8a61505c41d74768c0facee00
Maxim Chukitu <maxim.chukitu@codwer.com>
2019-09-16 19:32:38 +0300

    create simple signalR component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+import { HubConnection } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pad-lab1';
+export class AppComponent implements OnInit {
+  topic: string;
+
+  connection: HubConnection;
+  messages: string[] = [];
+
+  message: string;
+  isReady: boolean;
+
+  ngOnInit() {
+    this.connection = new HubConnectionBuilder()
+      .withUrl('https://localhost:5001/api/hub')
+      .build();
+
+    this.connection.on('connect', (messages: string) => {
+      this.messages = JSON.parse(messages);
+    });
+
+    this.connection.on('message', (message: string) => {
+      this.messages.push(message);
+    });
+
+    this.connection.on('error', (errorMessage: string) => {
+      alert(errorMessage);
+      this.connection = undefined;
+    });
+
+    this.connection.start()
+      .then(() => this.isReady = true)
+      .catch(console.log);
+  }
+
+  onTopicSubmit() {
+    this.connection.send('subscribe', this.topic).catch(console.log);
+  }
+
+  onMessageSubmit() {
+    this.connection.send('sendMessage', this.message, this.topic).catch(console.log);
+  }
 }
